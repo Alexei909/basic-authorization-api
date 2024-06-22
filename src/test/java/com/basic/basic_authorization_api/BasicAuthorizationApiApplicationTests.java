ccd2dabe49f46f21caa746497b27cec39fa358f8
Alexei909 <vegetables912@gmail.com>
2024-06-22 20:18:45 +0500

    init

---
+++ src/test/java/com/basic/basic_authorization_api/BasicAuthorizationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.basic_authorization_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthorizationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
